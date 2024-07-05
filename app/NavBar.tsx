4fd1bde26d8e55b258420b2d08f57648c575bd75
qabbes <quentin.abbes@gmail.com>
2024-07-05 14:32:29 +0200

    added classname dependencies + styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
-import { IoBugSharp } from "react-icons/io5";
 import { FaBug } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const navLinks = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
-    { label: "TBD", href: "/" },
+    { label: "TBD", href: "/tbd" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -16,7 +21,14 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {navLinks.map((link) => (
-          <Link key={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-blue-800": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}>
             {link.label}
           </Link>
         ))}
