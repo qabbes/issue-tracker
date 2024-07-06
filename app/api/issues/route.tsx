9afc02cce5f8f32f3a52087945c0b3dc9f6ce296
qabbes <quentin.abbes@gmail.com>
2024-07-06 22:35:31 +0200

    added client side validation

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import z from "zod";
+import { createIssueSchema } from "../../validationSchemas";
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required").max(255),
-  description: z.string().min(1, "Description is required"),
-});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
@@ -14,7 +9,7 @@ export async function POST(request: NextRequest) {
   const validation = createIssueSchema.safeParse(body);
 
   if (!validation.success) {
-    return NextResponse.json(validation.error.format(), { status: 400 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,17 +6,24 @@ import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
-import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { ExclamationTriangleIcon, InfoCircledIcon } from "@radix-ui/react-icons";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import z from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const [error, setError] = useState("");
   //set up the Hook Form
-  const { register, handleSubmit, control } = useForm<IssueForm>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   // set up the autosave feature for the SimpleMDE editor
   const router = useRouter();
   const delay = 1000;
@@ -57,6 +64,14 @@ const NewIssuePage = () => {
           }
         })}>
         <TextField.Root {...register("title")} placeholder="Title" />
+        {errors.title && (
+          <Callout.Root color="red">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>{errors.title.message}</Callout.Text>
+          </Callout.Root>
+        )}
         <Controller
           name="description"
           control={control}
@@ -64,6 +79,14 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} id="issue" value={autosavedValue} options={anOptions} />
           )}
         />
+        {errors.description && (
+          <Callout.Root color="red">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>{errors.description.message}</Callout.Text>
+          </Callout.Root>
+        )}
         <Button className="bg-blue-500 text-white p-2 rounded">Submit new issue</Button>
       </form>
     </div>
