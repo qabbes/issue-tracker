a2203cbde50c4e23f23dde6bcbe50852052eaf6c
qabbes <quentin.abbes@gmail.com>
2024-08-02 16:59:44 +0200

    updated PATCH in api/issue for assigning issue to user

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,22 +1,29 @@
 import authOptions from "@/app/auth/authOptions";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { createIssueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
-  const session = await getServerSession(authOptions);
+  /* const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  } */
 
   const body = await request.json();
   // Validate the incoming request body
-  const validation = createIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
-
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+  }
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -25,7 +32,12 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
   }
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description, status: body.status },
+    data: {
+      title: body.title,
+      description: body.description,
+      status: body.status,
+      assignedToUserId: body.assignedToUserId,
+    },
   });
 
   return NextResponse.json(updatedIssue, { status: 201 });
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,34 @@
 import z from "zod";
 
+
+//prettier-ignore
 export const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required").max(255),
-  description: z.string().min(1, { message: "Description is required" }),
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255),
+  description: z
+    .string()
+    .min(1, { message: "Description is required" })
+    .max(65535),
+});
+
+ //prettier-ignore
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255)
+    .optional(),
+  description: z
+    .string()
+    .min(1, { message: "Description is required" })
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, { message: "Assignee is required" })
+    .max(255)
+    .optional()
+    .nullable(),
 });
