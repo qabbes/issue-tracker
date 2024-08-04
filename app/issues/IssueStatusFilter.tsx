6699a607cfea5539b88543f5fbdf5590cce0ef7e
qabbes <quentin.abbes@gmail.com>
2024-08-04 14:48:36 +0200

    Filter issues by status

---
+++ app/issues/IssueStatusFilter.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+//prettier-ignore
+const statuses: { label: string, value?: Status }[] = [
+  {label: "All"},
+  {label: "Open", value: "OPEN"},
+  {label: "In Progress",value: "IN_PROGRESS"},
+  {label: "Closed", value: "CLOSED"},
+];
+
+const IssueStatusFilter = () => {
+  const [currentStatus, setCurrentStatus] = useState("");
+  const router = useRouter();
+
+  return (
+    <Select.Root
+      onValueChange={(status: string) => {
+        const query = status === "ALL" ? "" : `?status=${status}`;
+        router.push(`/issues/${query}`);
+        setCurrentStatus(status);
+      }}>
+      <Select.Trigger placeholder="Filter by status..."></Select.Trigger>
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.value} value={status.value ?? "ALL"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,9 +2,21 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssuesToolbar from "./IssuesToolbar";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  //check for typo in query params and returned undefined if status is not a valid status
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status: status },
+    orderBy: { createdAt: "desc" },
+  });
 
   return (
     <div>
