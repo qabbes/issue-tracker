7996a6b5859571e326a374470a1b705c2439fec4
qabbes <quentin.abbes@gmail.com>
2024-08-05 13:30:15 +0200

    implemented pagination layout

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,7 +1,7 @@
+"use client";
 import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
-import { MdKeyboardDoubleArrowLeft, MdOutlineDoubleArrow } from "react-icons/md";
-import { RxDoubleArrowLeft } from "react-icons/rx";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   itemsCount: number;
@@ -10,6 +10,15 @@ interface Props {
 }
 
 const Pagination = ({ currentPage, pageSize, itemsCount }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   const pageCount = Math.ceil(itemsCount / pageSize);
   if (pageCount <= 1) return null;
   return (
@@ -17,16 +26,31 @@ const Pagination = ({ currentPage, pageSize, itemsCount }: Props) => {
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" variant="ghost" radius="full" disabled={currentPage === 1}>
+      <Button color="gray" variant="ghost" radius="full" disabled={currentPage === 1} onClick={() => changePage(1)}>
         <DoubleArrowLeftIcon />
       </Button>
-      <Button color="gray" variant="ghost" radius="full" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="ghost"
+        radius="full"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}>
         <ChevronLeftIcon />
       </Button>
-      <Button color="gray" variant="ghost" radius="full" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="ghost"
+        radius="full"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}>
         <ChevronRightIcon />
       </Button>
-      <Button color="gray" variant="ghost" radius="full" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="ghost"
+        radius="full"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}>
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
+import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <div>Hello Issue Tracker!</div>;
+export default function Home({ searchParams }: { searchParams: { page: string } }) {
+  return <Pagination itemsCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />;
 }
