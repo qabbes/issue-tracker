cc9f4ad5bc7904133791c1892653cd37902b2b56
qabbes <quentin.abbes@gmail.com>
2024-08-06 15:27:39 +0200

    added latest issue component

---
+++ app/LatestIssues.tsx
@@ -0,0 +1,58 @@
+import prisma from "@/prisma/client";
+import { Avatar, Box, Card, Flex, Heading, Table } from "@radix-ui/themes";
+import React from "react";
+import { IssueStatusBadge } from "./components";
+import Link from "next/link";
+
+const LatestIssues = async () => {
+  const latestIssues = await prisma.issue.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  return (
+    <>
+      <Heading size="3" mb="2">
+        Latest Issues
+      </Heading>
+      <Card>
+        <Box pb="1">
+          <Table.Root>
+            <Table.Body>
+              {latestIssues.map((issue) => (
+                <Table.Row key={issue.id}>
+                  <Table.RowHeaderCell>
+                    <Flex justify="between">
+                      <Flex direction="column" align="start" gap="2">
+                        <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                        <Flex>
+                          <IssueStatusBadge status={issue.status} />
+                        </Flex>
+                      </Flex>
+                      <Box pt="0.6rem">
+                        {issue.assignedToUserId && (
+                          <Avatar
+                            size="2"
+                            radius="full"
+                            src={issue.assignedToUser?.image!}
+                            fallback={issue.assignedToUser?.name!.charAt(0)!}></Avatar>
+                        )}
+                      </Box>
+                    </Flex>
+                  </Table.RowHeaderCell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        </Box>
+      </Card>
+    </>
+  );
+};
+
+export default LatestIssues;
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import Pagination from "./components/Pagination";
 import PaginationSizeSelector from "./components/PaginationSizeSelector";
 import { Flex } from "@radix-ui/themes";
 import PaginationRadioSelector from "./components/PaginationRadioSelector";
+import LatestIssues from "./LatestIssues";
 
 export default function Home({ searchParams }: { searchParams: { page: string } }) {
-  return (
-    <>
-      <Flex align="center" gap="3"></Flex>
-    </>
-  );
+  return <LatestIssues />;
 }
